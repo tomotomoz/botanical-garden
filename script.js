a5692a6114955ca794c52f252185d6bfc356a1ea
tomotomoz <tomoko.essvivh@gmail.com>
2024-05-24 01:15:59 +0900

    レスポンシブnavArea修正

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 const targets = document.getElementsByClassName('fade');
 for(let i = targets.length; i--;){
 let observer = new IntersectionObserver((entries, observer) => {
@@ -13,3 +12,19 @@ let observer = new IntersectionObserver((entries, observer) => {
 });
 observer.observe(targets[i]);
 }
+
+const nav = document.querySelector('.nav-area')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', function() {
+  nav.classList.toggle('active')
+});
+
+const  navList = document.querySelectorAll('.nav > ul > li')
+console.log(navList)
+
+navList.forEach (el => {
+  el.addEventListener('click', function() {
+    nav.classList.remove('active')
+  });
+});
